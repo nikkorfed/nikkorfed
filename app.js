997564eb680b828203214dd705d9bcc83189ca31
Никита Корнилов <nikkorfed@gmail.com>
2020-09-13 21:04:45 +0500

    Подключены SSL сертификаты

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
 const app = express();
-const port = 80;
 
 app.use(express.static("public"));
 
@@ -17,6 +19,10 @@ app.use((req, res) => {
   res.status(404).sendFile("404.html", { root: "public" });
 });
 
-app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}...`);
-});
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/nikkorfed.ru/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/nikkorfed.ru/cert.pem"),
+};
+
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
