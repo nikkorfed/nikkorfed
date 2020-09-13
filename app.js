ccc84b992a50d557e568130e28089104593d7d63
Никита Корнилов <nikkorfed@gmail.com>
2020-09-14 00:20:02 +0500

    Добавлено приветствие, улучшен алгоритм изменения слов

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ app.use(express.json());
 
 app.use("/api/ruslan", (req, res) => {
   const word = req.body.request.nlu.tokens[req.body.request.nlu.tokens.length - 1];
-  let answer = "ху" + word;
-  res.json({
-    response: { text: answer, end_session: false },
-    version: "1.0",
-  });
+  let answer;
+
+  if (!word) answer = "Хорошо! Теперь я буду вести себя как Руслан. Скажите мне любое слово.";
+  else {
+    word = word.slice(1);
+    if (word[0] == "е") answer = "хуе" + word.slice(1);
+    else answer = "хуе" + word;
+  }
+
+  res.json({ response: { text: answer, end_session: false }, version: "1.0" });
 });
 
 app.use((req, res) => {
