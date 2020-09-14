21e981be45e2c05792dde10dff3b848a38ebd779
Никита Корнилов <nikkorfed@gmail.com>
2020-09-14 10:49:24 +0500

    Исправлены ошибки

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.all("/api/ruslan", (req, res) => {
 
     if (word[0] == "а" || word[0] == "я") answer = "хуя" + word.slice(1);
     else if (word[0] == "е" || word[0] == "э") answer = "хуе" + word.slice(1);
-    else if (word[0] == "ё" || word[0] == "о") answer = "ху" + word;
+    else if (word[0] == "ё" || word[0] == "о") answer = "хуи" + word;
     else if (word[0] == "и" || word[0] == "й") answer = "хуи" + word.slice(1);
     else if (word[0] == "у" || word[0] == "ю") answer = "хую" + word.slice(1);
     else answer = "хуе" + word;
