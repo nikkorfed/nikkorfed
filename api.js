1efeaa4284e83b3c49b4cb05778909644d15471e
Никита Корнилов <nikkorfed@gmail.com>
2020-09-15 03:47:56 +0500

    API вынесено в отдельный модуль

---
+++ api.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+
+router.use(express.json());
+
+router.use((req, res, next) => {
+  console.log("Время:", new Date().toLocaleString("en-GB"));
+  console.log(req.body);
+  next();
+});
+
+router.all("/ruslan", (req, res) => {
+  let word = req.body.request.nlu.tokens[req.body.request.nlu.tokens.length - 1];
+  let answer;
+
+  let consonants = "бвгджзклмнпрстфхцчшщ";
+  let vowels = "аеёийоуыэюя";
+
+  if (!word) answer = "Теперь я буду вести себя как Руслан. Скажите мне любое слово.";
+  else {
+    if (consonants.includes(word[0])) word = word.slice(1);
+    if (consonants.includes(word[0])) word = word.slice(1);
+
+    if (word[0] == "а" || word[0] == "я") answer = "хуя" + word.slice(1);
+    else if (word[0] == "е" || word[0] == "э") answer = "хуе" + word.slice(1);
+    else if (word[0] == "ё" || word[0] == "о") answer = "хуи" + word;
+    else if (word[0] == "и" || word[0] == "й") answer = "хуи" + word.slice(1);
+    else if (word[0] == "у" || word[0] == "ю") answer = "хую" + word.slice(1);
+    else answer = "хуе" + word;
+  }
+
+  res.json({ response: { text: answer, tts: `${answer.slice(0, 2)} ${answer.slice(2)}`, end_session: false }, version: "1.0" });
+});
+
+router.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send("Что-то пошло не так...");
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -4,41 +4,10 @@ const http = require("http");
 const fs = require("fs");
 const app = express();
 
+const api = require("./api");
+app.use("/api", api);
+
 app.use(express.static("public"));
-app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log(req.body);
-  next();
-});
-
-app.all("/api/ruslan", (req, res) => {
-  let word = req.body.request.nlu.tokens[req.body.request.nlu.tokens.length - 1];
-  let answer;
-
-  let consonants = "бвгджзклмнпрстфхцчшщ";
-  let vowels = "аеёийоуыэюя";
-
-  if (!word) answer = "Теперь я буду вести себя как Руслан. Скажите мне любое слово.";
-  else {
-    if (consonants.includes(word[0])) word = word.slice(1);
-    if (consonants.includes(word[0])) word = word.slice(1);
-
-    if (word[0] == "а" || word[0] == "я") answer = "хуя" + word.slice(1);
-    else if (word[0] == "е" || word[0] == "э") answer = "хуе" + word.slice(1);
-    else if (word[0] == "ё" || word[0] == "о") answer = "хуи" + word;
-    else if (word[0] == "и" || word[0] == "й") answer = "хуи" + word.slice(1);
-    else if (word[0] == "у" || word[0] == "ю") answer = "хую" + word.slice(1);
-    else answer = "хуе" + word;
-  }
-
-  res.json({ response: { text: answer, tts: `${answer.slice(0, 2)} ${answer.slice(2)}`, end_session: false }, version: "1.0" });
-});
-
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send("Что-то пошло не так...");
-});
 
 app.use((req, res) => {
   res.status(404).sendFile("404.html", { root: "public" });
