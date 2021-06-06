f1e5274d38cf839e5b81ccde383d20bd88b60a86
Никита Корнилов <nikkorfed@gmail.com>
2021-06-06 19:43:14 +0500

    Update app start script.

--- app.js
+++ app.js
@@ -4,21 +4,12 @@ const http = require("http");
 const fs = require("fs");
 const app = express();
 
-const api = require("./api");
-app.use("/api", api);
+const PORT = process.env.PORT || 3000;
+
+app.use("/api", require("./api"));
 
 app.use(express.static("public"));
 
 app.use((req, res) => res.status(404).sendFile("404.html", { root: "public" }));
 
-const options = {};
-
-try {
-  options.key = fs.readFileSync("/etc/letsencrypt/live/nikkorfed.ru/privkey.pem");
-  options.cert = fs.readFileSync("/etc/letsencrypt/live/nikkorfed.ru/cert.pem");
-} catch {}
-
-http.createServer(app).listen(80);
-https.createServer(options, app).listen(443, () => {
-  console.log("Сервер запущен...");
-});
+app.listen(PORT, () => console.log(`Сервер успешно запущен на порту ${PORT}...`));
